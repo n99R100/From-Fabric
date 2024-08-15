4fa8a4431169f7795492d114cffbca0ff8cc3456
Nandana Rathnayaka <86419173+n99R100@users.noreply.github.com>
2024-08-15 16:14:16 +0530

    Committing 2 items from workspace cfd7654a-1ac0-46ba-83e1-7c5abb6dc4c1

---
+++ Dev/DataflowsStagingWarehouse.Warehouse/dbo/Tables/03fa552511dd400fbd15923ad215fc12_7d4ee27b_002D70a9_002D4725_002Db4ea_002D882e360ec702.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[03fa552511dd400fbd15923ad215fc12_7d4ee27b_002D70a9_002D4725_002Db4ea_002D882e360ec702] (
+
+	[Column1] varchar(8000) NULL, 
+	[Column2] varchar(8000) NULL, 
+	[Column3] varchar(8000) NULL, 
+	[Column4] varchar(8000) NULL, 
+	[Column5] varchar(8000) NULL
+);
+
---
+++ Dev/DataflowsStagingWarehouse.Warehouse/dbo/Tables/03fa552511dd400fbd15923ad215fc12_b3de3cbd_002D7f71_002D4090_002Dbb41_002De02bcb541964.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[03fa552511dd400fbd15923ad215fc12_b3de3cbd_002D7f71_002D4090_002Dbb41_002De02bcb541964] (
+
+	[Column1] varchar(8000) NULL, 
+	[Column2] float NULL, 
+	[Column3] float NULL, 
+	[Column4] varchar(8000) NULL, 
+	[Column5] varchar(8000) NULL
+);
+
